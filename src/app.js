82e3e11bcdfc5839b943b0a21b43365461f5b93e
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-02-05 22:41:31 +0300

    game build complete | to be improvised

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector('.grid')
+const scoreDisplay = document.querySelector('#score')
 const blockWidth = 100;
 const blockHeight = 20;
 const boardWidth = 560;
@@ -12,8 +13,9 @@ const ballStart = [270, 40]
 let ballCurrentPosition = ballStart
 
 let timerId
-let xDirection = 2;
+let xDirection = -2;
 let yDirection = 2;
+let score = 0;
 
 class Block {
     constructor(xAxis, yAxis){
@@ -111,6 +113,28 @@ timerId = setInterval(moveBall, 30)
 
 // check for collisions
 function checkForCollisions(){
+    // check for block collisions
+    for (let i = 0; i < blocks.length; i++){
+        if (
+            (ballCurrentPosition[0] > blocks[i].bottomLeft[0] &&
+                ballCurrentPosition[0] < blocks[i].bottomRight[0]) && ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1])
+        ){
+            const allBlocks = Array.from(document.querySelectorAll('.block'))
+            allBlocks[i].classList.remove('block')
+            blocks.splice(i, 1)
+            changeDirection()
+            score++
+            scoreDisplay.innerHTML = score
+
+            // check for win
+            if (blocks.length === 0){
+                scoreDisplay.innerHTML = 'YOU WIN!'
+                clearInterval(timerId)
+                document.removeEventListener('keydown', moveUser)
+            }
+        }
+    }
+
     // check for wall collisions
     if (
         ballCurrentPosition[0] >= (boardWidth - ballDiameter) ||
@@ -118,6 +142,23 @@ function checkForCollisions(){
         ballCurrentPosition[0] <= 0
     ){
         changeDirection()
+    }
+
+    // check for user collisions
+    if
+    (
+      (ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blockWidth) &&
+      (ballCurrentPosition[1] > currentPosition[1] && ballCurrentPosition[1] < currentPosition[1] + blockHeight ) 
+    )
+    {
+      changeDirection()
+    }
+
+    // check for game over
+    if (ballCurrentPosition[1] <= 0) {
+        clearInterval(timerId)
+        scoreDisplay.innerHTML = 'You Lose!'
+        document.removeEventListener('keydown', moveUser)
     }
 }
 
